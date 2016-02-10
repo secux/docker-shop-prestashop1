35379f6171f351149950997c700156c81f5d5539
Mircea Silviu Itu <decemvre@gmail.com>
2016-02-10 17:19:32 +0200

    mod +x

--- data/install.sh
+++ data/install.sh
@@ -2,11 +2,10 @@
 
 echo "Running install.sh for $1"
 
-# TODO: Download selected version via  (from docker-sho-manager) from githup.com/opencart/opencart and run installer
+# TODO: Download selected version via  (from docker-sho-manager) from githup.com/prestashop/prestashop and run installer
 
-echo "Cloning opencart repository"
-cloneCmd="git clone --branch $1 --depth 1 https://2245dfb67d208f3503081b38814bdd54e04368f7:x-oauth-basic@github.com/PrestaShop/PrestaShop.git /www"
-eval cloneCmd
+echo "Cloning prestashop repository"
+git clone --branch $1 --depth 1 https://2245dfb67d208f3503081b38814bdd54e04368f7:x-oauth-basic@github.com/PrestaShop/PrestaShop.git /www
 
 echo "Installing composer packages"
 cd /www && composer.phar self-update && composer.phar config -g github-oauth.github.com 2245dfb67d208f3503081b38814bdd54e04368f7 && composer.phar install --ignore-platform-reqs
